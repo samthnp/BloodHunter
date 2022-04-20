1b149802539d5b93b9a59da5a27f5d0ce4d00daa
Sam <thanaponpdm@gmail.com>
2022-04-19 23:55:59 -0400

    Initial Commit

---
+++ NPCGameplayScenario/Assets/Scripts/BloodHunterAI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodHunterAI : MonoBehaviour
+{
+    public bool AffectedByFlashLight;
+    public bool SeePlayer;
+    public bool CatchPlayer;
+
+    public void StunnedByFlashLight()
+    {
+        // 
+    }
+}
